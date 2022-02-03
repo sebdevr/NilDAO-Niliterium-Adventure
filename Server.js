aea0138117b6724020794fc815172aff7057ab62
sebfk <76062949+sebfk@users.noreply.github.com>
2022-02-03 13:15:52 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -5,18 +5,10 @@ require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use(require("./record"));
+app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
  
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'mappinapp', 'build')));
-
-  app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'mappinapp', 'build', 'index.html'));
-  });
-}
-
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
