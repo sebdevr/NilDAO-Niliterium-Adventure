86848a8b0721171aab92690124aafa243c2b7938
sebfk <76062949+sebfk@users.noreply.github.com>
2022-02-03 20:08:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const dbo = require("./src/db/conn");
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
  
