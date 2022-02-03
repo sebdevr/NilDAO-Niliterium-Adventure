e97addb6ec185c69e8f996ec707045d154e88a87
sebfk <76062949+sebfk@users.noreply.github.com>
2022-02-03 10:20:08 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config({ path: "./src/config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/record"));
+app.use(require("./src/routes/record"));
 // get driver connection
-const dbo = require("./db/conn");
+const dbo = require("./src/db/conn");
  
 app.listen(port, () => {
   // perform a database connection when server starts
