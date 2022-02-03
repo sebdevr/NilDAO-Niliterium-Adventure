3bf78d7af602cb36860bb98672a2fdbb2a2d16f5
sebfk <76062949+sebfk@users.noreply.github.com>
2022-02-03 19:57:14 +0100

    test
    test

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const cors = require("cors");
 require("dotenv").config({ path: "./src/config.env" });
 const port = process.env.PORT || 5000;
@@ -8,6 +9,12 @@ app.use(express.json());
 app.use(require("./src/routes/record"));
 // get driver connection
 const dbo = require("./src/db/conn");
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
  
 app.listen(port, () => {
   // perform a database connection when server starts
@@ -16,4 +23,4 @@ app.listen(port, () => {
  
   });
   console.log(`Nil DAO - Server is running on port: ${port}`);
-});
+}); 
